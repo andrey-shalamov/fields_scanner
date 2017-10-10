a63f06f474acdde087175fa23e779e5648db6d65
andrey <andrey.shalamov@gmail.com>
2017-10-11 01:23:22 +0300

    big user_type_list stress test - fail

---
+++ test_compiler_03.cpp
@@ -0,0 +1,189 @@
+#include "fields_scanner_v2.h"
+
+struct S0
+{
+	int _1;
+};
+
+struct A00 {}; struct A01 {}; struct A02 {}; struct A03 {}; struct A04 {};
+struct A10 {}; struct A11 {}; struct A12 {}; struct A13 {};	struct A14 {};
+struct A20 {}; struct A21 {}; struct A22 {}; struct A23 {};	struct A24 {};
+struct A30 {}; struct A31 {}; struct A32 {}; struct A33 {};	struct A34 {};
+struct A40 {}; struct A41 {}; struct A42 {}; struct A43 {};	struct A44 {};
+struct A50 {}; struct A51 {}; struct A52 {}; struct A53 {};	struct A54 {};
+struct A60 {}; struct A61 {}; struct A62 {}; struct A63 {};	struct A64 {};
+struct A70 {}; struct A71 {}; struct A72 {}; struct A73 {};	struct A74 {};
+struct A80 {}; struct A81 {}; struct A82 {}; struct A83 {};	struct A84 {};
+struct A90 {}; struct A91 {}; struct A92 {}; struct A93 {};	struct A94 {};
+
+struct A05 {}; struct A06 {}; struct A07 {}; struct A08 {}; struct A09 {};
+struct A15 {}; struct A16 {}; struct A17 {}; struct A18 {};	struct A19 {};
+struct A25 {}; struct A26 {}; struct A27 {}; struct A28 {};	struct A29 {};
+struct A35 {}; struct A36 {}; struct A37 {}; struct A38 {};	struct A39 {};
+struct A45 {}; struct A46 {}; struct A47 {}; struct A48 {};	struct A49 {};
+struct A55 {}; struct A56 {}; struct A57 {}; struct A58 {};	struct A59 {};
+struct A65 {}; struct A66 {}; struct A67 {}; struct A68 {};	struct A69 {};
+struct A75 {}; struct A76 {}; struct A77 {}; struct A78 {};	struct A79 {};
+struct A85 {}; struct A86 {}; struct A87 {}; struct A88 {};	struct A89 {};
+struct A95 {}; struct A96 {}; struct A97 {}; struct A98 {};	struct A99 {};
+
+struct B00 {}; struct B01 {}; struct B02 {}; struct B03 {}; struct B04 {};
+struct B10 {}; struct B11 {}; struct B12 {}; struct B13 {};	struct B14 {};
+struct B20 {}; struct B21 {}; struct B22 {}; struct B23 {};	struct B24 {};
+struct B30 {}; struct B31 {}; struct B32 {}; struct B33 {};	struct B34 {};
+struct B40 {}; struct B41 {}; struct B42 {}; struct B43 {};	struct B44 {};
+struct B50 {}; struct B51 {}; struct B52 {}; struct B53 {};	struct B54 {};
+struct B60 {}; struct B61 {}; struct B62 {}; struct B63 {};	struct B64 {};
+struct B70 {}; struct B71 {}; struct B72 {}; struct B73 {};	struct B74 {};
+struct B80 {}; struct B81 {}; struct B82 {}; struct B83 {};	struct B84 {};
+struct B90 {}; struct B91 {}; struct B92 {}; struct B93 {};	struct B94 {};
+
+struct B05 {}; struct B06 {}; struct B07 {}; struct B08 {}; struct B09 {};
+struct B15 {}; struct B16 {}; struct B17 {}; struct B18 {};	struct B19 {};
+struct B25 {}; struct B26 {}; struct B27 {}; struct B28 {};	struct B29 {};
+struct B35 {}; struct B36 {}; struct B37 {}; struct B38 {};	struct B39 {};
+struct B45 {}; struct B46 {}; struct B47 {}; struct B48 {};	struct B49 {};
+struct B55 {}; struct B56 {}; struct B57 {}; struct B58 {};	struct B59 {};
+struct B65 {}; struct B66 {}; struct B67 {}; struct B68 {};	struct B69 {};
+struct B75 {}; struct B76 {}; struct B77 {}; struct B78 {};	struct B79 {};
+struct B85 {}; struct B86 {}; struct B87 {}; struct B88 {};	struct B89 {};
+struct B95 {}; struct B96 {}; struct B97 {}; struct B98 {};	struct B99 {};
+
+struct C00 {}; struct C01 {}; struct C02 {}; struct C03 {}; struct C04 {};
+struct C10 {}; struct C11 {}; struct C12 {}; struct C13 {};	struct C14 {};
+struct C20 {}; struct C21 {}; struct C22 {}; struct C23 {};	struct C24 {};
+struct C30 {}; struct C31 {}; struct C32 {}; struct C33 {};	struct C34 {};
+struct C40 {}; struct C41 {}; struct C42 {}; struct C43 {};	struct C44 {};
+struct C50 {}; struct C51 {}; struct C52 {}; struct C53 {};	struct C54 {};
+struct C60 {}; struct C61 {}; struct C62 {}; struct C63 {};	struct C64 {};
+struct C70 {}; struct C71 {}; struct C72 {}; struct C73 {};	struct C74 {};
+struct C80 {}; struct C81 {}; struct C82 {}; struct C83 {};	struct C84 {};
+struct C90 {}; struct C91 {}; struct C92 {}; struct C93 {};	struct C94 {};
+
+struct C05 {}; struct C06 {}; struct C07 {}; struct C08 {}; struct C09 {};
+struct C15 {}; struct C16 {}; struct C17 {}; struct C18 {};	struct C19 {};
+struct C25 {}; struct C26 {}; struct C27 {}; struct C28 {};	struct C29 {};
+struct C35 {}; struct C36 {}; struct C37 {}; struct C38 {};	struct C39 {};
+struct C45 {}; struct C46 {}; struct C47 {}; struct C48 {};	struct C49 {};
+struct C55 {}; struct C56 {}; struct C57 {}; struct C58 {};	struct C59 {};
+struct C65 {}; struct C66 {}; struct C67 {}; struct C68 {};	struct C69 {};
+struct C75 {}; struct C76 {}; struct C77 {}; struct C78 {};	struct C79 {};
+struct C85 {}; struct C86 {}; struct C87 {}; struct C88 {};	struct C89 {};
+struct C95 {}; struct C96 {}; struct C97 {}; struct C98 {};	struct C99 {};
+
+struct D00 {}; struct D01 {}; struct D02 {}; struct D03 {}; struct D04 {};
+struct D10 {}; struct D11 {}; struct D12 {}; struct D13 {};	struct D14 {};
+struct D20 {}; struct D21 {}; struct D22 {}; struct D23 {};	struct D24 {};
+struct D30 {}; struct D31 {}; struct D32 {}; struct D33 {};	struct D34 {};
+struct D40 {}; struct D41 {}; struct D42 {}; struct D43 {};	struct D44 {};
+struct D50 {}; struct D51 {}; struct D52 {}; struct D53 {};	struct D54 {};
+struct D60 {}; struct D61 {}; struct D62 {}; struct D63 {};	struct D64 {};
+struct D70 {}; struct D71 {}; struct D72 {}; struct D73 {};	struct D74 {};
+struct D80 {}; struct D81 {}; struct D82 {}; struct D83 {};	struct D84 {};
+struct D90 {}; struct D91 {}; struct D92 {}; struct D93 {};	struct D94 {};
+
+struct D05 {}; struct D06 {}; struct D07 {}; struct D08 {}; struct D09 {};
+struct D15 {}; struct D16 {}; struct D17 {}; struct D18 {};	struct D19 {};
+struct D25 {}; struct D26 {}; struct D27 {}; struct D28 {};	struct D29 {};
+struct D35 {}; struct D36 {}; struct D37 {}; struct D38 {};	struct D39 {};
+struct D45 {}; struct D46 {}; struct D47 {}; struct D48 {};	struct D49 {};
+struct D55 {}; struct D56 {}; struct D57 {}; struct D58 {};	struct D59 {};
+struct D65 {}; struct D66 {}; struct D67 {}; struct D68 {};	struct D69 {};
+struct D75 {}; struct D76 {}; struct D77 {}; struct D78 {};	struct D79 {};
+struct D85 {}; struct D86 {}; struct D87 {}; struct D88 {};	struct D89 {};
+struct D95 {}; struct D96 {}; struct D97 {}; struct D98 {};	struct D99 {};
+
+struct E00 {}; struct E01 {}; struct E02 {}; struct E03 {}; struct E04 {};
+struct E10 {}; struct E11 {}; struct E12 {}; struct E13 {};	struct E14 {};
+struct E20 {}; struct E21 {}; struct E22 {}; struct E23 {};	struct E24 {};
+struct E30 {}; struct E31 {}; struct E32 {}; struct E33 {};	struct E34 {};
+struct E40 {}; struct E41 {}; struct E42 {}; struct E43 {};	struct E44 {};
+struct E50 {}; struct E51 {}; struct E52 {}; struct E53 {};	struct E54 {};
+struct E60 {}; struct E61 {}; struct E62 {}; struct E63 {};	struct E64 {};
+struct E70 {}; struct E71 {}; struct E72 {}; struct E73 {};	struct E74 {};
+struct E80 {}; struct E81 {}; struct E82 {}; struct E83 {};	struct E84 {};
+struct E90 {}; struct E91 {}; struct E92 {}; struct E93 {};	struct E94 {};
+
+struct E05 {}; struct E06 {}; struct E07 {}; struct E08 {}; struct E09 {};
+struct E15 {}; struct E16 {}; struct E17 {}; struct E18 {};	struct E19 {};
+struct E25 {}; struct E26 {}; struct E27 {}; struct E28 {};	struct E29 {};
+struct E35 {}; struct E36 {}; struct E37 {}; struct E38 {};	struct E39 {};
+struct E45 {}; struct E46 {}; struct E47 {}; struct E48 {};	struct E49 {};
+struct E55 {}; struct E56 {}; struct E57 {}; struct E58 {};	struct E59 {};
+struct E65 {}; struct E66 {}; struct E67 {}; struct E68 {};	struct E69 {};
+struct E75 {}; struct E76 {}; struct E77 {}; struct E78 {};	struct E79 {};
+struct E85 {}; struct E86 {}; struct E87 {}; struct E88 {};	struct E89 {};
+struct E95 {}; struct E96 {}; struct E97 {}; struct E98 {};	struct E99 {};
+
+struct S
+{
+	S0 _1;
+};
+
+void compiler_stress_test_03()
+{
+	using namespace simple_reflection::v2;
+	using user_type_list = type_list<S0,
+
+		A00, A01, A02, A03, A04, A05, A06, A07, A08, A09,
+		A10, A11, A12, A13, A14, A15, A16, A17, A18, A19,
+		A20, A21, A22, A23, A24, A25, A26, A27, A28, A29,
+		A30, A31, A32, A33, A34, A35, A36, A37, A38, A39,
+		A40, A41, A42, A43, A44, A45, A46, A47, A48, A49,
+		A50, A51, A52, A53, A54, A55, A56, A57, A58, A59,
+		A60, A61, A62, A63, A64, A65, A66, A67, A68, A69,
+		A70, A71, A72, A73, A74, A75, A76, A77, A78, A79,
+		A80, A81, A82, A83, A84, A85, A86, A87, A88, A89,
+		A90, A91, A92, A93, A94, A95, A96, A97, A98, A99,
+
+		B00, B01, B02, B03, B04, B05, B06, B07, B08, B09,
+		B10, B11, B12, B13, B14, B15, B16, B17, B18, B19,
+		B20, B21, B22, B23, B24, B25, B26, B27, B28, B29,
+		B30, B31, B32, B33, B34, B35, B36, B37, B38, B39,
+		B40, B41, B42, B43, B44, B45, B46, B47, B48, B49,
+		B50, B51, B52, B53, B54, B55, B56, B57, B58, B59,
+		B60, B61, B62, B63, B64, B65, B66, B67, B68, B69,
+		B70, B71, B72, B73, B74, B75, B76, B77, B78, B79,
+		B80, B81, B82, B83, B84, B85, B86, B87, B88, B89,
+		B90, B91, B92, B93, B94, B95, B96, B97, B98, B99,
+
+		C00, C01, C02, C03, C04, C05, C06, C07, C08, C09,
+		C10, C11, C12, C13, C14, C15, C16, C17, C18, C19,
+		C20, C21, C22, C23, C24, C25, C26, C27, C28, C29,
+		C30, C31, C32, C33, C34, C35, C36, C37, C38, C39,
+		C40, C41, C42, C43, C44, C45, C46, C47, C48, C49,
+		C50, C51, C52, C53, C54, C55, C56, C57, C58, C59,
+		C60, C61, C62, C63, C64, C65, C66, C67, C68, C69,
+		C70, C71, C72, C73, C74, C75, C76, C77, C78, C79,
+		C80, C81, C82, C83, C84, C85, C86, C87, C88, C89,
+		C90, C91, C92, C93, C94, C95, C96, C97, C98, C99,
+
+		D00, D01, D02, D03, D04, D05, D06, D07, D08, D09,
+		D10, D11, D12, D13, D14, D15, D16, D17, D18, D19,
+		D20, D21, D22, D23, D24, D25, D26, D27, D28, D29,
+		D30, D31, D32, D33, D34, D35, D36, D37, D38, D39,
+		D40, D41, D42, D43, D44, D45, D46, D47, D48, D49,
+		D50, D51, D52, D53, D54, D55, D56, D57, D58, D59,
+		D60, D61, D62, D63, D64, D65, D66, D67, D68, D69,
+		D70, D71, D72, D73, D74, D75, D76, D77, D78, D79,
+		D80, D81, D82, D83, D84, D85, D86, D87, D88, D89,
+		D90, D91, D92, D93, D94, D95, D96, D97, D98, D99,
+
+		E00, E01, E02, E03, E04, E05, E06, E07, E08, E09,
+		E10, E11, E12, E13, E14, E15, E16, E17, E18, E19,
+		E20, E21, E22, E23, E24, E25, E26, E27, E28, E29,
+		E30, E31, E32, E33, E34, E35, E36, E37, E38, E39,
+		E40, E41, E42, E43, E44, E45, E46, E47, E48, E49,
+		E50, E51, E52, E53, E54, E55, E56, E57, E58, E59,
+		E60, E61, E62, E63, E64, E65, E66, E67, E68, E69,
+		E70, E71, E72, E73, E74, E75, E76, E77, E78, E79,
+		E80, E81, E82, E83, E84, E85, E86, E87, E88, E89,
+		E90, E91, E92, E93, E94, E95, E96, E97, E98, E99,
+
+		S0
+	>;
+	using user_template_type_list = template_type_list<>;
+	using serializer_t = aggregate_serializer<S, user_type_list, user_template_type_list>;
+	using types = serializer_t::fields_type_list_t;
+	static_assert(std::is_same<S0, type_list_element_t<0, types>>::value, "_!_");
+}
